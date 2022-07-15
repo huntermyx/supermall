87a059b4b29fca76d62f439bae01ac8fe36ad333
Hunter <hunter_myx@163.com>
2022-07-15 08:22:17 +0800

    首页搭建

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,31 @@
+<!--  -->
+<template>
+  <div class="featur">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="@/assets/img/home/recommend_bg.jpg"
+           alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {},
+  data() {
+    return {}
+  },
+  components: {},
+  computed: {},
+  beforeMount() {},
+  mounted() {},
+  methods: {},
+  watch: {}
+}
+</script>
+
+<style scoped>
+.featur img {
+  width: 100%;
+}
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,41 @@
+<!--  -->
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners">
+      <a :href="item.link">
+        <img :src="item.image">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from '@/components/common/swiper'
+
+export default {
+  name: '',
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {}
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  computed: {},
+  beforeMount() {},
+  mounted() {},
+  methods: {},
+  watch: {}
+}
+</script>
+
+<style lang='' scoped>
+</style>
